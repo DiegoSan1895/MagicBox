cfd50b9cd7a18f907754756fb1c2e4ebe840c8f4
SanDiego <xyq_since1895@163.com>
2016-03-13 10:05:26 +0800

    Show

---
+++ MagicBox/MagicBox/ShowStepGeniusViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ShowSetpGeniusViewController.swift
+//  MagicBox
+//
+//  Created by DiegoSan on 3/13/16.
+//  Copyright © 2016 DiegoSan. All rights reserved.
+//
+
+import UIKit
+
+class ShowStepGeniusViewController: ShowStepViewController {
+
+    
+}
