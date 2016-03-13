c2316f1f7de8c2429dde8f2f9db6c9562ca268da
SanDiego <xyq_since1895@163.com>
2016-03-13 22:38:01 +0800

    start to write animation

--- MagicBox/MagicBox/ShowStepGeniusViewController.swift
+++ MagicBox/MagicBox/ShowStepGeniusViewController.swift
@@ -10,5 +10,22 @@ import UIKit
 
 class ShowStepGeniusViewController: ShowStepViewController {
 
+    @IBOutlet private weak var dotsImageView: UIImageView!
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = NSLocalizedString("Genius", comment: "")
+        subtitleLabel.text = NSLocalizedString("Discover them around you", comment: "")
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let dotsPath = UIBezierPath(arcCenter: dotsImageView.center, radius: 7, startAngle: 0, endAngle: 2, clockwise: false)
+        repeatAnimate(dotsImageView, alongWithPath: dotsPath, duration: 7, autoreverses: true)
+    }
+    
     
 }
--- MagicBox/MagicBox/ShowStepViewController.swift
+++ MagicBox/MagicBox/ShowStepViewController.swift
@@ -10,26 +10,68 @@ import UIKit
 
 class ShowStepViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+
+    @IBOutlet weak var titleLabelBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var subtitleLabelBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        titleLabel.textColor = UIColor.yepTintColor()
+        
+        titleLabelBottomConstraint.constant = Ruler.iPhoneVertical(20, 30, 30, 30).value
+        subtitleLabelBottomConstraint.constant = Ruler.iPhoneVertical(120, 140, 160, 180).value
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        view.alpha = 0
     }
-    */
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
+            self.view.alpha = 1
+            }, completion: nil)
+        
+    }
+    
+    // MARK: - Animation Methods
+    
+    func repeatAnimate(view: UIView, alongWithPath path: UIBezierPath, duration: CFTimeInterval, autoreverses: Bool = false) {
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        
+        animation.calculationMode = kCAAnimationPaced
+        animation.fillMode = kCAFillModeForwards
+        animation.duration = duration
+        animation.repeatCount = Float.infinity
+        animation.autoreverses = autoreverses
+        
+        if autoreverses {
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+            
+        } else {
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        }
+        
+        animation.path = path.CGPath
+        
+        view.layer.addAnimation(animation, forKey: "Animation")
+    }
+    
+    func animate(view: UIView, offset: UInt32, duration: CFTimeInterval) {
+        
+        let path = UIBezierPath()
+        
+        func flip() -> CGFloat {
+            return arc4random() % 2 == 0 ? -1 : 1
+        }
+        
+        
+    }
 }
